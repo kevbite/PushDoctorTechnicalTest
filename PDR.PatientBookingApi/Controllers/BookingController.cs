0a30b80d260b1d1381ed3a4a551dd7b708bab6a8
Kevin Smith <kev_bite@msn.com>
2020-12-03 23:17:46 +0000

    Removing unused code.

--- PDR.PatientBookingApi/Controllers/BookingController.cs
+++ PDR.PatientBookingApi/Controllers/BookingController.cs
@@ -2,7 +2,6 @@
 using PDR.PatientBooking.Data;
 using PDR.PatientBooking.Data.Models;
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
@@ -89,30 +88,6 @@ namespace PDR.PatientBookingApi.Controllers
             public DateTime EndTime { get; set; }
             public long PatientId { get; set; }
             public long DoctorId { get; set; }
-        }
-
-        private static MyOrderResult UpdateLatestBooking(List<Order> bookings2, int i)
-        {
-            MyOrderResult latestBooking;
-            latestBooking = new MyOrderResult();
-            latestBooking.Id = bookings2[i].Id;
-            latestBooking.DoctorId = bookings2[i].DoctorId;
-            latestBooking.StartTime = bookings2[i].StartTime;
-            latestBooking.EndTime = bookings2[i].EndTime;
-            latestBooking.PatientId = bookings2[i].PatientId;
-            latestBooking.SurgeryType = (int) bookings2[i].GetSurgeryType();
-
-            return latestBooking;
-        }
-
-        private class MyOrderResult
-        {
-            public Guid Id { get; set; }
-            public DateTime StartTime { get; set; }
-            public DateTime EndTime { get; set; }
-            public long PatientId { get; set; }
-            public long DoctorId { get; set; }
-            public int SurgeryType { get; set; }
         }
     }
 }
